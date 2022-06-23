5d1865821a2eef4670ca23530e81a921c5f79853
ArmandoVVV <ie727115@iteso.mx>
2022-06-23 02:40:36 -0600

    Initial commit

---
+++ assembly_code/Test_Program.asm
@@ -0,0 +1,9 @@
+MAIN:
+	addi t1,zero,3		# 0 + 3 = 3
+	addi t2,zero,7		# 0 + 7 = 7
+	addi t3,zero,1		# 0 + 1 = 1
+	
+	add t1,t1,t1		# 3 + 3 = 6
+	add t2,t2,t1		# 7 + 6 = 13
+	add t3,t2,t1		# 13 + 6 = 19
+
